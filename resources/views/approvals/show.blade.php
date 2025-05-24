c685769f955a3a20606c89f8b55bcdaef1b85c76
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-05-24 14:14:14 +0300

    fix: show approval levels

--- resources/views/approvals/show.blade.php
+++ resources/views/approvals/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <!-- Hero -->
-    <div class="bg-body-light">
+    <div class="bg-body-light mt-5">
         <div class="content content-full">
             <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center py-2">
                 <div class="flex-grow-1">
@@ -23,7 +23,7 @@
     <!-- END Hero -->
 
     <!-- Page Content -->
-    <div class="content">
+    <div class="content p-2">
         <!-- Approval Details Block -->
         <div class="block block-rounded">
             <div class="block-header block-header-default">
