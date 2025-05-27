9c2a034e7d27ea7d54c868be265ce0780b7891d7
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-05-27 23:04:40 +0300

    fix: trip

--- app/Models/Trip.php
+++ app/Models/Trip.php
@@ -2,29 +2,31 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Allocation;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Trip extends Model
 {
     use HasFactory;
+    // for relationship
+    public function allocation()
+    {
+        return $this->belongsTo(Allocation::class, 'allocation_id', 'id');
+    }
 
-    protected $fillable = [
-        'ref_no',
-        'allocation_id',
-        'initiated_date',
-        'total_payment',
-        'status',
-        'state',
-        'created_by',
-        'type',
-        'approval_status',
-        'completion_approval_status',
-        'invoiced_income',
-    ];
+    // public function final_inspection()
+    // {
+    //     return $this->hasOne(FinalInspection::class, 'trip_id', 'id');
+    // }
 
-    protected $casts = [
-        'initiated_date' => 'date',
-        'status' => 'boolean',
-    ];
+    // public function brakedown()
+    // {
+    //     return $this->hasOne(Breakdown::class, 'trip_id', 'id');
+    // }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'created_by', 'id');
+    }
 }
