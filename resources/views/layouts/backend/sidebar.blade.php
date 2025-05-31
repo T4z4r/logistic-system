dfea1a54398da7d445ece07638e8db51e5ae4306
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-05-31 20:08:08 +0300

    fix: settings activeness

--- resources/views/layouts/backend/sidebar.blade.php
+++ resources/views/layouts/backend/sidebar.blade.php
@@ -274,50 +274,59 @@
                  </li>
              </ul>
          </li>
-
-         <li class="nav-main-item{{ request()->is('system-configurations*', 'roles.*') ? ' open' : '' }}">
-             <a class="nav-main-link nav-main-link-submenu" data-toggle="submenu" aria-haspopup="true"
-                 aria-expanded="{{ request()->is('system-configurations*') ? 'true' : 'false' }}" href="#">
+         @php
+             $isSystemSettings = request()->is('system-configurations*') ||
+                                 request()->is('departments*') ||
+                                 request()->is('positions*') ||
+                                 request()->is('user-roles*') ||
+                                 request()->is('roles*') ||
+                                 request()->is('permissions*') ||
+                                 request()->is('approvals*');
+         @endphp
+         <li class="nav-main-item{{ $isSystemSettings ? ' open' : '' }}">
+             <a class="nav-main-link nav-main-link-submenu{{ $isSystemSettings ? ' active' : '' }}" data-toggle="submenu" aria-haspopup="true"
+                 aria-expanded="{{ $isSystemSettings ? 'true' : 'false' }}" href="#">
                  <i class="nav-main-link-icon fa fa-cogs"></i>
                  <span class="nav-main-link-name">System Settings</span>
              </a>
              <ul class="nav-main-submenu">
                  <li class="nav-main-item">
-                     <a class="nav-main-link{{ request()->is('system-configurations/general', 'roles.*') ? ' active' : '' }}"
+                     <a class="nav-main-link{{ request()->is('system-configurations/general') ? ' active' : '' }}"
                          href="{{ route('blank') }}">
                          <i class="nav-main-link-icon si si-settings"></i>
                          <span class="nav-main-link-name">General Settings</span>
                      </a>
+                 </li>
                  <li class="nav-main-item">
-                     <a class="nav-main-link{{ request()->is('departments.*') ? ' active' : '' }}"
+                     <a class="nav-main-link{{ request()->is('departments*') ? ' active' : '' }}"
                          href="{{ route('departments.index') }}">
                          <i class="nav-main-link-icon si si-settings"></i>
                          <span class="nav-main-link-name">Departments</span>
                      </a>
                  </li>
                  <li class="nav-main-item">
-                     <a class="nav-main-link{{ request()->is('positions.*') ? ' active' : '' }}"
+                     <a class="nav-main-link{{ request()->is('positions*') ? ' active' : '' }}"
                          href="{{ route('positions.index') }}">
                          <i class="nav-main-link-icon si si-settings"></i>
                          <span class="nav-main-link-name">Positions</span>
                      </a>
                  </li>
                  <li class="nav-main-item">
-                     <a class="nav-main-link{{ request()->is('user-roles.*') ? ' active' : '' }}"
+                     <a class="nav-main-link{{ request()->is('user-roles*') ? ' active' : '' }}"
                          href="{{ route('users-roles.index') }}">
                          <i class="nav-main-link-icon si si-users"></i>
                          <span class="nav-main-link-name">User Roles</span>
                      </a>
                  </li>
                  <li class="nav-main-item">
-                     <a class="nav-main-link{{ request()->is('roles.*') ? ' active' : '' }}"
+                     <a class="nav-main-link{{ request()->is('roles*') ? ' active' : '' }}"
                          href="{{ route('roles.index') }}">
                          <i class="nav-main-link-icon si si-user-following"></i>
                          <span class="nav-main-link-name">Roles</span>
                      </a>
                  </li>
                  <li class="nav-main-item">
-                     <a class="nav-main-link{{ request()->is('permissions.*') ? ' active' : '' }}"
+                     <a class="nav-main-link{{ request()->is('permissions*') ? ' active' : '' }}"
                          href="{{ route('permissions.index') }}">
                          <i class="nav-main-link-icon si si-lock"></i>
                          <span class="nav-main-link-name">Permissions</span>
@@ -338,6 +347,7 @@
                      </a>
                  </li>
              </ul>
+         </li>
          <li class="nav-main-item">
              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                  @csrf
