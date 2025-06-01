98ec6aabf4fad05bd73e87372263451c0fbef65f
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-06-01 12:58:47 +0300

    fix: minor

--- resources/views/trailers/index.blade.php
+++ resources/views/trailers/index.blade.php
@@ -158,7 +158,7 @@
     @foreach ($trailers as $trailer)
         <div class="modal fade" id="assignTruckModal{{ $trailer->id }}" tabindex="-1"
             aria-labelledby="assignTruckModalLabel{{ $trailer->id }}" aria-hidden="true">
-            <div class="modal-dialog">
+            <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="assignTruckModalLabel{{ $trailer->id }}">Assign Truck to Trailer:
