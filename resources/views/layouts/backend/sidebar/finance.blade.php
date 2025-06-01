58c11c6ec310fc1c06f77932f29e7e43f418b175
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-06-01 23:47:12 +0300

    fix: finance management

--- resources/views/layouts/backend/sidebar/finance.blade.php
+++ resources/views/layouts/backend/sidebar/finance.blade.php
@@ -1,76 +1,71 @@
-<li class="nav-main-heading">Accounts Management</li>
-  
-         <li class="nav-main-item">
-             <a class="nav-main-link{{ request()->is('trip-settings/costs') ? ' active' : '' }}"
-                 href="/trip-settings/costs">
-                 <i class="nav-main-link-icon fa fa-truck"></i>
-                 <span class="nav-main-link-name">
-                     Trips
-                 </span>
-             </a>
-         </li>
-         <li class="nav-main-item">
-             <a class="nav-main-link{{ request()->is('trip-settings/costs') ? ' active' : '' }}"
-                 href="/trip-settings/costs">
-                 <i class="nav-main-link-icon fa fa-calculator"></i>
-                 <span class="nav-main-link-name">
-                     Trip Expenses
-                 </span>
-             </a>
-         </li>
-         <li class="nav-main-item">
-             <a class="nav-main-link{{ request()->is('trip-settings/costs') ? ' active' : '' }}"
-                 href="/trip-settings/costs">
-                 <i class="nav-main-link-icon fa fa-gas-pump"></i>
-                 <span class="nav-main-link-name">
-                     Fuel Procurements
-                 </span>
-             </a>
-         </li>
-         <li class="nav-main-item{{ request()->is('client-payments*') ? ' open' : '' }}">
-             <a class="nav-main-link nav-main-link-submenu" data-toggle="submenu" aria-haspopup="true"
-                 aria-expanded="{{ request()->is('client-payments*') ? 'true' : 'false' }}" href="#">
-                 <i class="nav-main-link-icon fa fa-credit-card"></i>
-                 <span class="nav-main-link-name">Client Payments</span>
-             </a>
-             <ul class="nav-main-submenu">
-                 <li class="nav-main-item">
-                     <a class="nav-main-link{{ request()->is('client-payments/invoices') ? ' active' : '' }}"
-                         href="/client-payments/invoices">
-                         <i class="nav-main-link-icon fa fa-file-invoice"></i>
-                         <span class="nav-main-link-name">Trip Invoices</span>
-                     </a>
-                 </li>
-                 <li class="nav-main-item">
-                     <a class="nav-main-link{{ request()->is('client-payments/debit-notes') ? ' active' : '' }}"
-                         href="/client-payments/debit-notes">
-                         <i class="nav-main-link-icon fa fa-file-invoice-dollar"></i>
-                         <span class="nav-main-link-name">Debit Notes</span>
-                     </a>
-                 </li>
-                 <li class="nav-main-item">
-                     <a class="nav-main-link{{ request()->is('client-payments/credit-notes') ? ' active' : '' }}"
-                         href="/client-payments/credit-notes">
-                         <i class="nav-main-link-icon fa fa-file-invoice"></i>
-                         <span class="nav-main-link-name">Credit Notes</span>
-                     </a>
-                 </li>
-                 <li class="nav-main-item">
-                     <a class="nav-main-link{{ request()->is('client-payments/standing-charges') ? ' active' : '' }}"
-                         href="/client-payments/standing-charges">
-                         <i class="nav-main-link-icon fa fa-money-bill"></i>
-                         <span class="nav-main-link-name">Standing Charges</span>
-                     </a>
-                 </li>
-             </ul>
-         </li>
-         <li class="nav-main-item">
-             <a class="nav-main-link{{ request()->is('trip-settings/costs') ? ' active' : '' }}"
-                 href="/trip-settings/costs">
-                 <i class="nav-main-link-icon fa fa-file-pdf"></i>
-                 <span class="nav-main-link-name">
-                     Reports
-                 </span>
-             </a>
-         </li>
+<li class="nav-main-heading">Finance Management</li>
 
+<li class="nav-main-item">
+    <a class="nav-main-link{{ request()->is('trip-settings/costs') ? ' active' : '' }}" href="/trip-settings/costs">
+        <i class="nav-main-link-icon fa fa-truck text-blue"></i>
+        <span class="nav-main-link-name">
+            Trips
+        </span>
+    </a>
+</li>
+<li class="nav-main-item">
+    <a class="nav-main-link{{ request()->is('trip-settings/costs') ? ' active' : '' }}" href="/trip-settings/costs">
+        <i class="nav-main-link-icon fa fa-calculator text-orange"></i>
+        <span class="nav-main-link-name">
+            Trip Expenses
+        </span>
+    </a>
+</li>
+<li class="nav-main-item">
+    <a class="nav-main-link{{ request()->is('trip-settings/costs') ? ' active' : '' }}" href="/trip-settings/costs">
+        <i class="nav-main-link-icon fa fa-gas-pump text-brown"></i>
+        <span class="nav-main-link-name">
+            Fuel Procurements
+        </span>
+    </a>
+</li>
+<li class="nav-main-item{{ request()->is('client-payments*') ? ' open' : '' }}">
+    <a class="nav-main-link nav-main-link-submenu" data-toggle="submenu" aria-haspopup="true"
+        aria-expanded="{{ request()->is('client-payments*') ? 'true' : 'false' }}" href="#">
+        <i class="nav-main-link-icon fa fa-credit-card text-green"></i>
+        <span class="nav-main-link-name">Client Payments</span>
+    </a>
+    <ul class="nav-main-submenu">
+        <li class="nav-main-item">
+            <a class="nav-main-link{{ request()->is('client-payments/invoices') ? ' active' : '' }}"
+                href="/client-payments/invoices">
+                <i class="nav-main-link-icon fa fa-file-invoice"></i>
+                <span class="nav-main-link-name">Trip Invoices</span>
+            </a>
+        </li>
+        <li class="nav-main-item">
+            <a class="nav-main-link{{ request()->is('client-payments/debit-notes') ? ' active' : '' }}"
+                href="/client-payments/debit-notes">
+                <i class="nav-main-link-icon fa fa-file-invoice-dollar"></i>
+                <span class="nav-main-link-name">Debit Notes</span>
+            </a>
+        </li>
+        <li class="nav-main-item">
+            <a class="nav-main-link{{ request()->is('client-payments/credit-notes') ? ' active' : '' }}"
+                href="/client-payments/credit-notes">
+                <i class="nav-main-link-icon fa fa-file-invoice"></i>
+                <span class="nav-main-link-name">Credit Notes</span>
+            </a>
+        </li>
+        <li class="nav-main-item">
+            <a class="nav-main-link{{ request()->is('client-payments/standing-charges') ? ' active' : '' }}"
+                href="/client-payments/standing-charges">
+                <i class="nav-main-link-icon fa fa-money-bill"></i>
+                <span class="nav-main-link-name">Standing Charges</span>
+            </a>
+        </li>
+    </ul>
+</li>
+<li class="nav-main-item">
+    <a class="nav-main-link{{ request()->is('trip-settings/costs') ? ' active' : '' }}" href="/trip-settings/costs">
+        <i class="nav-main-link-icon fa fa-file-pdf text-blue"></i>
+        <span class="nav-main-link-name">
+            Finance Reports
+        </span>
+    </a>
+</li>
