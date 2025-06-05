195b7299f0c3f4d525548d6f95d2542378e05cda
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-06-05 09:05:39 +0300

    fix: godown stock

--- app/Models/GodownStock.php
+++ app/Models/GodownStock.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class GodownStock extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['stock_item_id', 'godown_id', 'quantity', 'company_id'];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function stockItem()
+    {
+        return $this->belongsTo(StockItem::class);
+    }
+
+    public function godown()
+    {
+        return $this->belongsTo(Godown::class);
+    }
 }
