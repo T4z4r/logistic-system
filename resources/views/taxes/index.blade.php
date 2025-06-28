19eda3d7638cbdbe1c7cb3471b84951ced89f404
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-06-28 21:38:38 +0300

    fix: content

--- resources/views/taxes/index.blade.php
+++ resources/views/taxes/index.blade.php
@@ -2,7 +2,7 @@
 @extends('layouts.backend')
 
 @section('content')
-    <div class="content">
+    <div class="content1 p-2">
         @include('partials._notifications')
 
         <div class="block block-rounded">
