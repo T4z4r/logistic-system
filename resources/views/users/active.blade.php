909f25b9357bec89e06d2ba59dc22dfd4d628250
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-07-03 04:17:39 +0300

    feat: edit user modal

--- resources/views/users/active.blade.php
+++ resources/views/users/active.blade.php
@@ -104,9 +104,12 @@
                                     <a href="{{ route('users.show', $user) }}" class="btn btn-sm btn-alt-primary">
                                         <i class="fa fa-list"></i>
                                     </a>
-                                    <a href="{{ route('users.edit', $user) }}" class="btn btn-sm btn-alt-primary">
+                                    <!-- Trigger Edit Modal -->
+                                    <button type="button" class="btn btn-sm btn-alt-primary" data-bs-toggle="modal"
+                                        data-bs-target="#editUserModal{{ $user->id }}">
                                         <i class="fa fa-edit"></i>
-                                    </a>
+                                    </button>
+
                                     <form action="{{ route('users.destroy', $user) }}" method="POST"
                                         style="display: inline;">
                                         @csrf
@@ -118,8 +121,102 @@
                                     </form>
                                 </td>
                             </tr>
+
+                            <!-- Edit Modal -->
+                            <div class="modal fade" id="editUserModal{{ $user->id }}" tabindex="-1"
+                                aria-labelledby="editUserModalLabel{{ $user->id }}" aria-hidden="true">
+                                <div class="modal-dialog modal-xl">
+                                    <div class="modal-content">
+                                        <form action="{{ route('users.update', $user->id) }}" method="POST">
+                                            @csrf
+                                            @method('PUT')
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="editUserModalLabel{{ $user->id }}">Edit
+                                                    User - {{ $user->name }}</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+
+                                                <div class="row">
+                                                    <div class="col-md-6 mb-4">
+                                                        <label class="form-label">Name</label>
+                                                        <input type="text" name="name" value="{{ $user->name }}"
+                                                            class="form-control" required>
+                                                    </div>
+                                                    <div class="col-md-6 mb-4">
+                                                        <label class="form-label">Email</label>
+                                                        <input type="email" name="email" value="{{ $user->email }}"
+                                                            class="form-control" required>
+                                                    </div>
+                                                </div>
+
+                                                <div class="row">
+                                                    <div class="col-md-6 mb-4">
+                                                        <label class="form-label">Department</label>
+                                                        <select name="department_id" class="form-control">
+                                                            <option value="">Select Department</option>
+                                                            @foreach ($departments as $department)
+                                                                <option value="{{ $department->id }}"
+                                                                    {{ $user->department_id == $department->id ? 'selected' : '' }}>
+                                                                    {{ $department->name }}
+                                                                </option>
+                                                            @endforeach
+                                                        </select>
+                                                    </div>
+                                                    <div class="col-md-6 mb-4">
+                                                        <label class="form-label">Position</label>
+                                                        <select name="position_id" class="form-control">
+                                                            <option value="">Select Position</option>
+                                                            @foreach ($positions as $position)
+                                                                <option value="{{ $position->id }}"
+                                                                    {{ $user->position_id == $position->id ? 'selected' : '' }}>
+                                                                    {{ $position->name }}
+                                                                </option>
+                                                            @endforeach
+                                                        </select>
+                                                    </div>
+                                                </div>
+
+                                                <div class="row">
+                                                    <div class="col-md-6 mb-4">
+                                                        <label class="form-label">Line Manager</label>
+                                                        <select name="line_manager_id" class="form-control">
+                                                            <option value="">Select Line Manager</option>
+                                                            @foreach ($managers as $manager)
+                                                                <option value="{{ $manager->id }}"
+                                                                    {{ $user->line_manager_id == $manager->id ? 'selected' : '' }}>
+                                                                    {{ $manager->name }}
+                                                                </option>
+                                                            @endforeach
+                                                        </select>
+                                                    </div>
+                                                    <div class="col-md-6 mb-4">
+                                                        <label class="form-label">Status</label>
+                                                        <select name="status" class="form-control">
+                                                            <option value="1"
+                                                                {{ $user->status == 1 ? 'selected' : '' }}>Active</option>
+                                                            <option value="0"
+                                                                {{ $user->status == 0 ? 'selected' : '' }}>Inactive
+                                                            </option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-alt-secondary"
+                                                    data-bs-dismiss="modal">Cancel</button>
+                                                <button type="submit" class="btn btn-alt-primary">Update User</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End Edit Modal -->
                         @endforeach
                     </tbody>
+
                 </table>
                 {{-- {{ $users->links() }} --}}
             </div>
@@ -129,7 +226,8 @@
     <!-- END Page Content -->
 
     <!-- Create User Modal -->
-    <div class="modal fade" id="createUserModal" tabindex="-1" aria-labelledby="createUserModalLabel" aria-hidden="true">
+    <div class="modal fade" id="createUserModal" tabindex="-1" aria-labelledby="createUserModalLabel"
+        aria-hidden="true">
         <div class="modal-dialog modal-xl modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header bg-body-light">
@@ -153,7 +251,8 @@
                             <div class="col-md-6 mb-4">
                                 <label class="form-label" for="email">Email</label>
                                 <input type="email" name="email" id="email" placeholder="Enter email address"
-                                    class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}">
+                                    class="form-control @error('email') is-invalid @enderror"
+                                    value="{{ old('email') }}">
                                 @error('email')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
